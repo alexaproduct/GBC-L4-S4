6e4bd9b0379fb342c06d4e026f465f373ed16100
Alexandra Zotova <alexa@Alexandras-MacBook-Pro.local>
2023-02-18 16:46:18 +0600

    L4-1 added

---
+++ L4-1/Program.cs
@@ -0,0 +1,59 @@
+﻿// L1 Двумерные массивы и рекурсия
+
+//[строки, столбцы]
+// string[,] table = new string[2, 3];
+// table [1, 2] = "слово";
+
+// table[0,0]   table[0,1]
+// table[1,0]   table[1,1]
+
+// int[,] matrix = new int[5,8];
+
+// for (int rows = 0; rows < 2; rows++)
+// {
+//    for (int columns = 0; columns < 5; columns++)
+//    {
+//        Console.WriteLine($"-{table[rows, columns]}-");
+//    }
+//}
+
+// int[,] matrix = new int[3, 4];
+
+// for (int i = 0; i < 3; i++)
+// {
+//    for (int j = 0; j < 4; j++)
+//    {
+//        Console.Write($"{matrix[i, j]} ");
+//    }
+// Console.WriteLine();    
+// }
+
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+    Console.WriteLine();    
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10);
+        }
+    }
+}
+
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
