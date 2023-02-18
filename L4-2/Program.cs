a83f83dbc5addd01d4311c71eea41d3f144d31ab
Alexandra Zotova <alexa@Alexandras-MacBook-Pro.local>
2023-02-18 19:51:18 +0600

    L4-2 added

---
+++ L4-2/Program.cs
@@ -0,0 +1,31 @@
+﻿// L4-2 Рекурсия
+
+// Факториал
+
+double Factorial (int n)
+{
+    // 1! = 1
+    // 0! = 1
+    if (n == 1) return 1;
+    else return n * Factorial(n-1);
+}
+
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+
+Console.WriteLine();
+
+// Фибоначчи
+
+int Fibonacci(int n)
+{
+    if(n == 1 || n == 2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"F({i}) = {Fibonacci(i)}");
+}
